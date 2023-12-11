8b7668bb9636959ceae2949a079fb2fd383654f6
universopack.com <dksiao352@gmail.com>
2023-12-10 20:08:13 -0600

    Actualiza index.js con cambios específicos

--- src/index.js
+++ src/index.js
@@ -1,23 +1,46 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-async function obtenerUrlImagen(url) {
+async function obtenerUrlsCategoria(url) {
   try {
     const response = await axios.get(url);
     const html = response.data;
     const $ = cheerio.load(html);
 
-    const imageUrl = $('img').attr('src');
-    return imageUrl;
+    const imageUrls = [];
+
+    // Buscar en las etiquetas img dentro de figure
+    $('figure.wp-block-image img[src]').each((_, element) => {
+      imageUrls.push($(element).attr('src'));
+    });
+
+    return imageUrls;
   } catch (error) {
-    throw new Error(`Error al obtener la imagen (${url}): ${error.message}`);
+    throw new Error(`Error al obtener las imágenes (${url}): ${error.message}`);
+  }
+}
+
+async function obtenerUrlAleatoria(url) {
+  try {
+    const imageUrls = await obtenerUrlsCategoria(url);
+
+    if (imageUrls.length === 0) {
+      throw new Error('No se encontraron imágenes en la categoría.');
+    }
+
+    const randomIndex = Math.floor(Math.random() * imageUrls.length);
+    const randomImageUrl = imageUrls[randomIndex];
+
+    return randomImageUrl;
+  } catch (error) {
+    throw new Error(`Error al obtener la imagen aleatoria de la categoría (${url}): ${error.message}`);
   }
 }
 
 async function wantapiwaifu() {
   const url = 'https://wantbot.xyz/wantapiwaifu/';
   try {
-    const imageUrl = await obtenerUrlImagen(url);
+    const imageUrl = await obtenerUrlAleatoria(url);
     return imageUrl;
   } catch (error) {
     throw new Error(`Error al obtener la imagen de waifu: ${error.message}`);
@@ -27,42 +50,44 @@ async function wantapiwaifu() {
 async function wantapiass() {
   const url = 'https://wantbot.xyz/wantapiass/';
   try {
-    const imageUrl = await obtenerUrlImagen(url);
+    const imageUrl = await obtenerUrlAleatoria(url);
     return imageUrl;
   } catch (error) {
-    throw new Error(`Error al obtener la imagen de ass: ${error.message}`);
+    throw new Error(`Error al obtener la imagen de waifu: ${error.message}`);
   }
 }
 
 async function wantapibikinis() {
   const url = 'https://wantbot.xyz/wantapibikinis/';
   try {
-    const imageUrl = await obtenerUrlImagen(url);
+    const imageUrl = await obtenerUrlAleatoria(url);
     return imageUrl;
   } catch (error) {
-    throw new Error(`Error al obtener la imagen de ass: ${error.message}`);
+    throw new Error(`Error al obtener la imagen de waifu: ${error.message}`);
   }
 }
 
 async function wantapidog() {
   const url = 'https://wantbot.xyz/wantapidog/';
   try {
-    const imageUrl = await obtenerUrlImagen(url);
+    const imageUrl = await obtenerUrlAleatoria(url);
     return imageUrl;
   } catch (error) {
-    throw new Error(`Error al obtener la imagen de ass: ${error.message}`);
+    throw new Error(`Error al obtener la imagen de waifu: ${error.message}`);
   }
 }
 
 async function wantapiwaifuexotica() {
   const url = 'https://wantbot.xyz/wantapiwaifuexotica/';
   try {
-    const imageUrl = await obtenerUrlImagen(url);
+    const imageUrl = await obtenerUrlAleatoria(url);
     return imageUrl;
   } catch (error) {
-    throw new Error(`Error al obtener la imagen de ass: ${error.message}`);
+    throw new Error(`Error al obtener la imagen de waifu: ${error.message}`);
   }
 }
+
+// Otras funciones de categoría...
 
 module.exports = {
   wantapiwaifu,
